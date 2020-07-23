4bf03f25550bca2d53b873d820491ab4b4de5415
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-07-23 17:48:51 +0600

    Cache guides meta data

--- guides/api.js
+++ guides/api.js
@@ -2,9 +2,32 @@ import fs from 'fs'
 import path from 'path'
 import json from '../index.json'
 
+const cache = indexedData()
+
+function indexedData() {
+  const cached = {}
+
+  json.forEach(({ title, guides }) => {
+    guides.forEach(item => {
+      const image = item.image_url.replace(/^guides\//, '/guide/')
+      const slug = item.file_url.replace(/^guides\//, '')
+
+      cached[slug] = {
+        image,
+        title: item.title,
+        date: item.updated_at,
+        type: title
+      }
+    })
+  })
+
+  return cached
+}
+
 export function getGuideBySlug(slug, fields = []) {
   const fullPath = path.join(process.cwd(), 'guides', slug)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
+  const cached = cache[slug]
 
   const items = {}
 
@@ -15,6 +38,10 @@ export function getGuideBySlug(slug, fields = []) {
 
     if (field === 'content') {
       items[field] = fileContents
+    }
+
+    if (cached[field]) {
+      items[field] = cached[field]
     }
   })
 
