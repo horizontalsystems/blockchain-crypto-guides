220c39aaa5cae384d4684106d075aeaa7093d439
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-07-29 11:07:33 +0600

    Select active menu section on scroll

--- components/Markdown/Markdown.js
+++ components/Markdown/Markdown.js
@@ -1,8 +1,42 @@
 import React from 'react'
 import GithubSlugger from 'github-slugger'
+import throttle from 'lodash.throttle'
 import Container from '../Container'
 
 class Markdown extends React.Component {
+  componentDidMount() {
+    window.addEventListener('scroll', throttle(this.handleScroll, 100));
+
+    this.mainNavLinks = document.querySelectorAll('.Markdown-menu a');
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const fromTop = window.scrollY;
+    const actives = []
+
+    this.mainNavLinks.forEach(link => {
+      const section = document.querySelector(link.hash)
+      if (!section) return
+      if (section.offsetTop <= fromTop) {
+        actives.push(link)
+      } else {
+        link.classList.remove('current');
+      }
+    })
+
+    actives.forEach((link, index) => {
+      if (actives.length - 1 === index) {
+        link.classList.add('current');
+      } else {
+        link.classList.remove('current');
+      }
+    })
+  }
+
   mapMenu(headings) {
     const slugger = new GithubSlugger()
     const menu = []
@@ -11,9 +45,13 @@ class Markdown extends React.Component {
       let item = headings[i]
       let value
 
+      if (!item.children) {
+        continue
+      }
+
       const child = item.children[0]
       const slug = slugger.slug(child.value)
-      const link = <a href={`#${slug}`}>{child.value}</a>
+      const link = <a href={`#${slug}`} onClick={this.onClickLink}>{child.value}</a>
       switch (item.depth) {
         case 2:
           value = <div className="pl-3">{link}</div>
@@ -36,13 +74,22 @@ class Markdown extends React.Component {
     return menu
   }
 
-  toggleMenu() {
+  onClickToggle() {
     const { display } = this.menu.style
-    if (display === 'none') {
+    if (display === 'none' || display === '') {
       this.menu.style.display = 'block'
     } else {
       this.menu.style.display = 'none'
     }
+  }
+
+  onClickLink(event) {
+    event.preventDefault();
+    const target = document.querySelector(event.target.hash);
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
   }
 
   render() {
@@ -56,7 +103,7 @@ class Markdown extends React.Component {
         />
         <div className="Markdown-section Markdown-side">
           <div className="Markdown-side-content">
-            <div className="Markdown-side-title Markdown-menu-bordered" onClick={() => this.toggleMenu()}>
+            <div className="Markdown-side-title Markdown-menu-bordered" onClick={() => this.onClickToggle()}>
               Contents
             </div>
             <div className="Markdown-menu" ref={r => this.menu = r}>
