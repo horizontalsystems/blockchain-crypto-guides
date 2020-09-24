6222a477df8ea3a0f163f52291c27f2c147968e0
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-09-24 12:05:00 +0600

    Add guide `id` to props

--- website/pages/[lang]/index.js
+++ website/pages/[lang]/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import Home from '../../components/Home/Home'
 import getAllGuides from '../../api/guides-api'
-import { getI18nPaths, getI18nProps, withI18n } from '../../i18n'
+import { getI18nPaths, getI18nProps } from '../../i18n'
 
 export async function getStaticProps(ctx) {
   const i18nProps = await getI18nProps(ctx, ['common'])
   const guides = getAllGuides([
+    'id',
+    'type',
     'title',
     'date',
     'image',
-    'slug',
-    'type'
+    'slug'
   ], i18nProps.lang)
 
   return {
