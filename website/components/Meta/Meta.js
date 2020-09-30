4614a34aa61a5eadc51607ff0ccbe1d25abe9493
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-09-30 12:08:10 +0600

    Add SEO Meta Tags and robots.txt

--- website/components/Meta/Meta.js
+++ website/components/Meta/Meta.js
@@ -1,37 +1,37 @@
+import React from 'react'
 import Head from 'next/head'
 
 export default function Meta() {
   return (
     <Head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
+      <title>Litrex Academy</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta name="description" content="Master fundamentals and learn about crypto projects in simple terms" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="manifest" href="/favicon/manifest.json" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&family=Noto+Sans&display=swap" />
-      <title>Academy</title>
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
-      <meta name="description" content="A statically generated blog example for Litrex academy" />
-      <meta property="og:image" content="/image.png" />
       <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js" />
-      <script dangerouslySetInnerHTML={{
-        __html: 'function onSuccess(id) {\n' +
-          '          $(`#mlb2-${id} .Contact-success`).show();\n' +
-          '          $(`#mlb2-${id} .Contact-form`).hide();\n' +
-          '      }\n' +
-          '\n' +
-          '      function ml_webform_success_2166978() {\n' +
-          '          onSuccess(2166978)\n' +
-          '      }\n' +
-          '\n' +
-          '      function ml_webform_success_1561498() {\n' +
-          '          onSuccess(1561498)\n' +
-          '      }'
-      }} />
+      <script dangerouslySetInnerHTML={{ __html: mlScript() }} />
     </Head>
+  )
+}
+
+function mlScript() {
+  return (
+    'function onSuccess(id) {' +
+    '  $(`#mlb2-${id} .Contact-success`).show();' +
+    '  $(`#mlb2-${id} .Contact-form`).hide();' +
+    '}\n' +
+    'function ml_webform_success_2166978() {' +
+    '  onSuccess(2166978)' +
+    '}\n' +
+    'function ml_webform_success_1561498() {' +
+    '  onSuccess(1561498)' +
+    '}'
   )
 }
